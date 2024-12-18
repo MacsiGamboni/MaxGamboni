b32cfcf4212303cc5763a5162d8efd80b8dd7547
MacsiGamboni <117485957+MacsiGamboni@users.noreply.github.com>
2024-12-18 12:39:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const scrollToTopButton = document.getElementById('scrollToTop');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        scrollToTopButton.style.display = 'block'; 
+    } else {
+        scrollToTopButton.style.display = 'none'; 
+    }
+});
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' 
+    });
+});
